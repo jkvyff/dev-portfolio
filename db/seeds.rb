2d93676fe2927f7407c8e7f1a0279072084de358
jkvyff <kristianvyff@gmail.com>
2020-10-07 20:14:56 -0700

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blogNum|
+  Blog.create!(
+    title: "My Blog Post number #{blogNum}",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  )
+end
+
+puts "10 blog posts created"
+
+4.times do |skillNum|
+  Skill.create!(
+    title: "Rails #{skillNum}",
+    percent_utilized: 25 
+  )
+end
+
+puts "4 skills created"
+
+9.times do |projectNum|
+  Project.create!(
+    title: "Rails #{projectNum}",
+    subtitle: "Great Project",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident",
+    main_image: "https://placehold.it/700x400",
+    thumb_image: "https://placeholde.it/350x200"
+  )
+end
+
+puts "9 projects created"

2757c9b9e9521f92c16540236d98e5be90637398
jkvyff <kristianvyff@gmail.com>
2020-10-07 21:14:05 -0700

    added update functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,45 @@ class ProjectsController < ApplicationController
   def index
     @portfolio_projects = Project.all
   end
+
+  def new
+    @portfolio_project = Project.new
+  end
+
+  def create
+    @portfolio_project = Project.new(project_params)
+
+    respond_to do |format|
+      if @portfolio_project.save
+        format.html { redirect_to projects_path, notice: 'Project was successfully created.' }
+        format.json { render :show, status: :created, location: @portfolio_project }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio_project.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+    @portfolio_project = Project.find(params[:id])
+  end
+
+  def update
+    @portfolio_project = Project.find(params[:id])
+    respond_to do |format|
+      if @portfolio_project.update(project_params)
+        format.html { redirect_to projects_path, notice: 'Project was successfully updated.' }
+        format.json { render :show, status: :ok, location: @portfolio_project }
+      else
+        format.html { render :edit}
+        format.json { render json: @portfolio_project.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  private
+    def project_params
+      params.require(:project).permit(:title, :subtitle, :body)
+    end
 end

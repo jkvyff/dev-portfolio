a14d9047dd5f72823976d0b67f0e9c7526ff3465
jkvyff <kristianvyff@gmail.com>
2020-10-09 11:57:07 -0700

    added custom scopes to projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,9 @@
 class Project < ApplicationRecord
-  validates_presence_of :title, :percent_utilized
+  validates_presence_of :title, :body, :main_image, :thumb_image
+
+  def self.ror
+    where(subtitle: 'Ruby on Rails')
+  end
+
+  scope :ruby_on_rails_project_items, -> { where(subtitle: 'Ruby on Rails') }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+3.times do |topicNum|
+  Topic.create!(
+    title: "Topic #{topicNum}"
+  )
+end
+
+puts "3 Topics created"
+
+
 10.times do |blogNum|
   Blog.create!(
     title: "My Blog Post number #{blogNum}",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    topic_id: Topic.last.id
   )
 end
 
 puts "10 blog posts created"
+
 
 4.times do |skillNum|
   Skill.create!(
@@ -23,10 +27,21 @@ end
 
 puts "4 skills created"
 
-9.times do |projectNum|
+
+7.times do |projectNum|
   Project.create!(
     title: "Project Number: #{projectNum}",
-    subtitle: "Great Project",
+    subtitle: "Ruby on Rails",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident",
+    main_image: "https://placehold.it/700x400",
+    thumb_image: "https://placehold.it/350x200"
+  )
+end
+
+2.times do |projectNum|
+  Project.create!(
+    title: "Project Number: #{projectNum}",
+    subtitle: "Node js",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident",
     main_image: "https://placehold.it/700x400",
     thumb_image: "https://placehold.it/350x200"

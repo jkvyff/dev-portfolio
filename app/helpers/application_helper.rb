91a4fcef6e756e8efb634aea092c960d7dc0dab1
jkvyff <kristianvyff@gmail.com>
2020-10-14 18:36:23 -0700

    added link to profile pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
   def login_helper style = ''
     if !current_user.is_a?(GuestUser)
-      link_to "Logout", destroy_user_session_path, method: :delete, class: style
+      (link_to "Profile", edit_user_registration_path, class: style) + " " +
+      (link_to "Logout", destroy_user_session_path, method: :delete, class: style)
     else
       (link_to "Register", new_user_registration_path, class: style) + " " +
       (link_to "Login", new_user_session_path, class: style)

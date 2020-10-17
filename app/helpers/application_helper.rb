7ff180976153ae96d4f88a8a2632f10b00f1417c
jkvyff <kristianvyff@gmail.com>
2020-10-16 19:04:30 -0700

    refactored nav for application, blog and projects

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,5 +18,48 @@ module ApplicationHelper
   def set_copyright
     "&copy; #{Time.now.year} | <b>Jens Vyff</b> , All rights reserved".html_safe
   end
-end
 
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: "Home"
+      },
+      {
+        url: projects_path,
+        title: "Projects"
+      },
+      {
+        url: blogs_path,
+        title: "Blog"
+      },
+      {
+        url: contact_path,
+        title: "Contact"
+      },
+      {
+        url: about_path,
+        title: "About"
+      },
+    ]
+  end
+
+  def nav_helper style, tag_type, tags = true
+    nav_links = ''
+    if tags == false
+      nav_items.each do |item|
+        nav_links << "<a href='#{item[:url]}' class='#{style} #{active? item[:url]}' >#{item[:title]}</a>"
+      end
+    else
+      nav_items.each do |item|
+        nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}' >#{item[:title]}</a></#{tag_type}>"
+      end
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
+end
